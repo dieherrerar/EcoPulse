9300b5055187a1a50152e2e4bf87bde2f3980a98
cristobal <cr.alvarezm@duocuc.cl>
2025-09-09 23:20:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="p-8">
       <h1 className="text-2xl font-bold">Hola Next.js + React 🚀</h1>
       <p>Mi primera página con App Router.</p>
+      <h1>VALLANCE TODOS A LA SHUSHA DE SU PIIIIIIIIII</h1>
     </main>
   );
 }
