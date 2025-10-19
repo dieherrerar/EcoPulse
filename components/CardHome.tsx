eb6d5e255c866403d218285770dd303260e57ea5
cristobal <cr.alvarezm@duocuc.cl>
2025-10-19 19:46:12 -0300

    conexion del proyecto desde equipos windows

--- components/CardHome.tsx
+++ components/CardHome.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./estilos_componentes.css";
-import React, { ReactNode } from "react";
+import React from "react";
 
 interface AccordionProps {
-  title: String;
-  description: String;
-  imageUrl?: String;
+  title: string;
+  description: string;
+  imageUrl?: string;
 }
 
-export default function Accordion(props: AccordionProps) {
-  const { title, description } = props;
+export default function Accordion({ title, description }: AccordionProps) {
+  // Cargar Bootstrap JS solo en el cliente
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <div className="card" style={{ width: "18rem", height: "15rem" }}>
       <div className="card-body">
