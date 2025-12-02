0da6d1c014461a70b9f7de17c91696c25d580dbb
Diego Herrera Rodríguez <diegoherrerarodriguez@Diegos-Mac-mini.local>
2025-12-02 00:51:03 -0300

    prueba de funcionalidad api

--- src/app/api/temperatura/concon/route.ts
+++ src/app/api/temperatura/concon/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
     const apiKey = process.env.APIKEY;
 
     if (!apiKey) {
+      console.error("METEOSOURCE APIKEY NO DEFINIDA EN EL ENTORNO");
       return NextResponse.json(
         { error: "Falta la Api key en el entorno" },
         { status: 500 }
@@ -15,26 +16,31 @@ export async function GET() {
     }
 
     const url = `${baseUrl}?lat=${lat}&lon=${lon}&sections=current,hourly&timezone=America/Santiago&language=en&units=metric&key=${apiKey}`;
+
+    console.log("Llamando a Meteosource con URL:", url);
+
     const res = await fetch(url, { cache: "no-store" });
 
     if (!res.ok) {
-      const err = await res.text();
-      throw new Error("Error al consultar Meteosource: ${err}");
+      const errText = await res.text();
+      console.error("Meteosource respondió con error:", res.status, errText);
+      throw new Error(
+        `Error al consultar Meteosource: ${res.status} ${errText}`
+      );
     }
 
     const data = await res.json();
 
-    const temperatura = data?.current?.temperature;
+    const temperatura = data?.current?.temperature ?? null;
     const summary = data?.current?.summary ?? null;
     const icon = data?.current?.icon ?? null;
 
     return NextResponse.json({ temperatura, summary, icon, lat, lon });
   } catch (error: any) {
-    console.error("Error Meteosource: ", error.messagge);
+    console.error("Error Meteosource:", error); // <--- aquí ya no se rompe
     return NextResponse.json(
       { error: "Error al obtener los datos" },
       { status: 500 }
     );
   }
 }
-
